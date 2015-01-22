8bb558f039775f2e9834a354b2df2d82c3e369e1
Kenneth Kouot <kenneth@wikia-inc.com>
2015-01-22 15:15:39 -0800

    switch from git add . to --all in update script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -7,7 +7,7 @@ env -i rm -rf *
 env -i git checkout dev -- gh-pages/
 env -i cp -R gh-pages/* ./
 env -i rm -rf gh-pages/
-env -i git add .
+env -i git add --all 
 env -i git commit -m 'Updated gh-pages from dev'
 env -i git push origin gh-pages
 env -i git checkout dev
