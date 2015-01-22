821af5eaf090158110c1f66d2bbfdb3d8903a62a
Kenneth Kouot <kenneth@wikia-inc.com>
2015-01-22 15:09:56 -0800

    update update script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+env -i git push origin :gh-pages
 env -i git branch -D gh-pages
-env -i git checkout --orphan gh-pages
+env -i git checkout -b gh-pages
+env -i rm -rf *
 env -i git checkout dev -- gh-pages/
 env -i cp -R gh-pages/* ./
 env -i git add .
