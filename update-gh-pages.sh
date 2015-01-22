092f86f108de45801cfcacfdc30c1bfc057ab0b0
Kenneth Kouot <kenneth@wikia-inc.com>
2015-01-22 15:12:21 -0800

    remove gh-pages in update script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -6,6 +6,7 @@ env -i git checkout -b gh-pages
 env -i rm -rf *
 env -i git checkout dev -- gh-pages/
 env -i cp -R gh-pages/* ./
+env -i rm -rf gh-pages/
 env -i git add .
 env -i git commit -m 'Updated gh-pages from dev'
 env -i git push origin gh-pages
