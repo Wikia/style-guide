b7a7fbda4878b286ff7d21040ae59a26de2032a5
Kenneth Kouot <kenneth@wikia-inc.com>
2015-01-27 14:25:49 -0800

    remove gh-pages folder _site (this is built by jekyll)

--- gh-pages/_site/vendor/wikia-style-guide/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass');
-
-gulp.task('sass', function () {
-	gulp.src('./src/scss/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('./dist/css'));
-});
