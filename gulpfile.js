febdc86772cf99c88eb442894a963d603f172801
lizlux <liz@lizlux.net>
2015-08-12 16:49:01 -0700

    HG-834 uglifying JS

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	del = require('del'),
 	gulpif = require('gulp-if'),
-	preprocess = require('gulp-preprocess');
+	preprocess = require('gulp-preprocess'),
+	uglify = require('gulp-uglify'),
+	rename = require("gulp-rename");
 
 gulp.task('sass', function () {
 	return gulp.src('./src/scss/**/*.scss')
@@ -52,27 +54,28 @@ gulp.task('dist:clean', function () {
  *  - Inline JS and CSS into template file
  *  - Move file to dist folder
  */
-gulp.task('components-sass', function () {
+gulp.task('components-assets', function () {
 	var base = './src/components';
 
-	return gulp.src(base + '/**/*.scss')
-		.pipe(
-			sass({outputStyle: 'compressed'})
-				.on('error', sass.logError)
-		)
+	return gulp.src(base + '/**/*.+(scss|js)')
+		.pipe(gulpif('*.scss', sass({outputStyle: 'compressed'}).on('error', sass.logError)))
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(rename(function (path) {
+			path.extname = '.min' + path.extname;
+		}))
 		.pipe(gulp.dest(base));
 });
 gulp.task('components-svg', function () {
 	return gulp.src('./src/components/**/*.svg')
 		.pipe(gulp.dest('./dist/components'))
 });
-gulp.task('components-html', ['components-sass'], function () {
+gulp.task('components-html', ['components-assets'], function () {
 	return gulp.src('./src/components/**/*.html')
 		.pipe(preprocess())
 		.pipe(gulp.dest('./dist/components'));
 });
 gulp.task('components', ['components-html', 'components-svg'], function () {
-	return del.sync('./src/components/**/*.css');
+	return del.sync('./src/components/**/*.min.+(css|js)');
 });
 
 gulp.task('update-static', [
